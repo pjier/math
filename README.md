1b64fc2ed1f48756f49d3da9e3e64bfdc1ff49ce
Per Isacson <per.isacson@gmail.com>
2025-12-23 16:42:18 +0100

    Add documentation

---
+++ README.md
@@ -0,0 +1,3 @@
+# Math
+
+Math is a simple go module that provides utility functions for doing arithmetic. It was created as a way to explore how to create go modules.
--- math.go
+++ math.go
@@ -1,5 +1,11 @@
+// The math package contains a few utility functions to do basic arithmetic.
 package math
 
+// Add adds two integers and returns the result.
+//
+// Read more about addition at [mathisfun].
+//
+// [mathsisfun]: https://www.mathsisfun.com/numbers/addition.html
 func Add(i, j int) int {
 	return i + j
 }
